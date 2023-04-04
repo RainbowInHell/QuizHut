0beb4c8360c3b9ce0b8bc1e371764e485e8e9364
Ssparf <sashaparfiak007@gmail.com>
2023-04-04 21:12:38 +0300

    fixed of usings

--- QuizHut/QuizHut/ViewModel/LoginViewModel.cs
+++ QuizHut/QuizHut/ViewModel/LoginViewModel.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuizHut.ViewModel
+﻿namespace QuizHut.ViewModel
 {
+    using System;
+
     class LoginViewModel
     {
     }
