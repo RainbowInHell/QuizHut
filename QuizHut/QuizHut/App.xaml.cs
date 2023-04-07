74561ce69de39a07fc52ec5a676a5f1aef68f022
Bykovsky Matvey <matveyrfh@mail.ru>
2023-04-07 19:40:32 +0300

    Remove unneccessary nuget packages

--- QuizHut/QuizHut/App.xaml.cs
+++ QuizHut/QuizHut/App.xaml.cs
@@ -1,12 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace QuizHut
 {
