b664c94833998df1b30d5918d6ad88ffcfeeb88c
Ssparf <sashaparfiak007@gmail.com>
2023-04-12 15:27:29 +0300

    Adding command for close application in LoginViewModel

--- QuizHut/QuizHut/ViewModels/LoginViewModel.cs
+++ QuizHut/QuizHut/ViewModels/LoginViewModel.cs
@@ -1,8 +1,31 @@
-﻿namespace QuizHut.ViewModel
+﻿namespace QuizHut.ViewModels
 {
+    using QuizHut.Infrastructure.Commands;
     using System;
+    using System.Windows;
+    using System.Windows.Input;
 
     class LoginViewModel
     {
+        #region Commands
+
+        #region CloseApplicationCommand
+
+        public ICommand CloseApplicationCommand { get; set; }
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p) { Application.Current.Shutdown(); }
+
+        #endregion
+
+        #endregion
+
+        public LoginViewModel() 
+        {
+            #region Commands
+
+            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
