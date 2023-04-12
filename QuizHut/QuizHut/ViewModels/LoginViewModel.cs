3898c811b393caae9ed21b125e3df84bb54695f8
Ssparf <sashaparfiak007@gmail.com>
2023-04-12 18:11:31 +0300

    Attaching resource dictionary to app, deleted commands from LoginViewModel, added regions for furure command

--- QuizHut/QuizHut/ViewModels/LoginViewModel.cs
+++ QuizHut/QuizHut/ViewModels/LoginViewModel.cs
@@ -1,25 +1,10 @@
 ﻿namespace QuizHut.ViewModels
 {
-    using QuizHut.Infrastructure.Commands;
-    using System;
-    using System.Windows;
-    using System.Windows.Input;
-
     class LoginViewModel
     {
         #region Commands
 
-        #region CloseApplicationCommand
-
-        public ICommand CloseApplicationCommand { get; set; }
-        private bool CanCloseApplicationCommandExecute(object p) => true;
-        private void OnCloseApplicationCommandExecuted(object p) { Application.Current.Shutdown(); }
-
-        #endregion
-
-        public ICommand MinimizeApplicationCommand { get; set; }
-        private bool CanMinimizeApplicationCommandExecute(object p) => true;
-        private void OnMinimizeApplicationCommandExecuted(object p) { Application.Current.Windows[0].WindowState = WindowState.Minimized; }
+        //commands
 
         #endregion
 
@@ -27,8 +12,7 @@
         {
             #region Commands
 
-            CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-            MinimizeApplicationCommand = new ActionCommand(OnMinimizeApplicationCommandExecuted, CanMinimizeApplicationCommandExecute);
+            //commands
 
             #endregion
         }
