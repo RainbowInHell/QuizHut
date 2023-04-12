5a7b1a2c7543019c0308a24d9f67ea138ba5d2e1
Ssparf <sashaparfiak007@gmail.com>
2023-04-12 20:14:38 +0300

    Fixed usings

--- QuizHut/QuizHut/Infrastructure/Commands/MinimizeApplicationCommand.cs
+++ QuizHut/QuizHut/Infrastructure/Commands/MinimizeApplicationCommand.cs
@@ -1,13 +1,8 @@
-﻿using QuizHut.Infrastructure.Commands.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace QuizHut.Infrastructure.Commands
+﻿namespace QuizHut.Infrastructure.Commands
 {
+    using QuizHut.Infrastructure.Commands.Base;
+    using System.Windows;
+
     class MinimizeApplicationCommand : Command
     {
         public override bool CanExecute(object? parameter) => true;
--- QuizHut/QuizHut/ViewModels/ChooseRoleViewModel.cs
+++ QuizHut/QuizHut/ViewModels/ChooseRoleViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace QuizHut.ViewModels
+﻿namespace QuizHut.ViewModels
 {
     class ChooseRoleViewModel
     {
