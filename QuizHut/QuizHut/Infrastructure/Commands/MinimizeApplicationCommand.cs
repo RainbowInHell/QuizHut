75a54235a06a03c240b07736678bfe04d1e695ea
Ssparf <sashaparfiak007@gmail.com>
2023-04-12 18:07:35 +0300

    Adding commands as resource dictionary and added to it commands for close and minimize

---
+++ QuizHut/QuizHut/Infrastructure/Commands/MinimizeApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using QuizHut.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuizHut.Infrastructure.Commands
+{
+    class MinimizeApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Windows[0].WindowState = WindowState.Minimized;
+    }
+}
