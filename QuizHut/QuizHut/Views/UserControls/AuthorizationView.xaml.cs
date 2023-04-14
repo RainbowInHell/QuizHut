297fdaf4c5275834ca46e37ba23175155ef1081c
Ssparf <sashaparfiak007@gmail.com>
2023-04-14 14:55:13 +0300

    Added show/hide password functionality

--- QuizHut/QuizHut/Views/UserControls/AuthorizationView.xaml.cs
+++ QuizHut/QuizHut/Views/UserControls/AuthorizationView.xaml.cs
@@ -16,20 +16,20 @@
             InitializeComponent();
         }
 
-        //private void AVShowHidePasswImg_MouseLeave(object sender, MouseEventArgs e)
-        //{
-        //    HidePassword();
-        //}
+        private void AVShowHidePasswImg_MouseLeave(object sender, MouseEventArgs e)
+        {
+            HidePassword();
+        }
 
-        //private void AVShowHidePasswImg_MouseDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    ShowPassword();
-        //}
+        private void AVShowHidePasswImg_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPassword();
+        }
 
-        //private void AVShowHidePasswImg_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        //{
-        //    HidePassword();
-        //}
+        private void AVShowHidePasswImg_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            HidePassword();
+        }
 
         //private void AVPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         //{
@@ -39,21 +39,21 @@
         //        AVShowHidePasswImg.Visibility = Visibility.Hidden;
         //}
 
-        //private void ShowPassword()
-        //{
-        //    AVShowHidePasswImg.Source = new BitmapImage(new Uri("pack://application:,,,/QuizHut;component/Images/LoginViewImages/EyePic2.png"));
-        //    AVVisiblePasswBox.Visibility = Visibility.Visible;
-        //    AVPasswordBox.Visibility = Visibility.Hidden;
-        //    //AVVisiblePasswBox.Text = AVPasswordBox.Password;
-        //}
+        private void ShowPassword()
+        {
+            AVShowHidePasswImg.Source = new BitmapImage(new Uri("pack://application:,,,/QuizHut;component/Images/LoginViewImages/EyePic2.png"));
+            AVVisiblePasswBox.Visibility = Visibility.Visible;
+            AVPasswordBox.Visibility = Visibility.Hidden;
+            AVVisiblePasswBox.Text = AVPasswordBox.Password;
+        }
 
-        //private void HidePassword()
-        //{
-        //    AVShowHidePasswImg.Source = new BitmapImage(new Uri("pack://application:,,,/QuizHut;component/Images/LoginViewImages/EyePic.png"));
-        //    AVVisiblePasswBox.Visibility = Visibility.Hidden;
-        //    AVPasswordBox.Visibility = Visibility.Visible;
-        //    AVPasswordBox.Focus();
-        //}
+        private void HidePassword()
+        {
+            AVShowHidePasswImg.Source = new BitmapImage(new Uri("pack://application:,,,/QuizHut;component/Images/LoginViewImages/EyePic.png"));
+            AVVisiblePasswBox.Visibility = Visibility.Hidden;
+            AVPasswordBox.Visibility = Visibility.Visible;
+            AVPasswordBox.Focus();
+        }
     }
 }
 
