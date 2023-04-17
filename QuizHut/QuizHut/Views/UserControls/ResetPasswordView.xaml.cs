6a9b722b4a62525c851ef617cb674779a6393117
Ssparf <sashaparfiak007@gmail.com>
2023-04-17 12:15:27 +0300

    Added ResetPasswordView and code formatted

---
+++ QuizHut/QuizHut/Views/UserControls/ResetPasswordView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuizHut.Views.UserControls
+{
+    using System.Windows.Controls;
+
+    public partial class ResetPasswordView : UserControl
+    {
+        public ResetPasswordView()
+        {
+            InitializeComponent();
+        }
+    }
+}
