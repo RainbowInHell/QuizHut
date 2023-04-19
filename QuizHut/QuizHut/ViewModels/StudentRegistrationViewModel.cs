190280697ea493080ef2e73439b21e5f1231eada
Ssparf <sashaparfiak007@gmail.com>
2023-04-19 12:54:53 +0300

    Added navigate to authorization command in StudentRegistrationView

--- QuizHut/QuizHut/ViewModels/StudentRegistrationViewModel.cs
+++ QuizHut/QuizHut/ViewModels/StudentRegistrationViewModel.cs
@@ -1,8 +1,36 @@
 ﻿namespace QuizHut.ViewModels
 {
+    using System.Windows.Input;
+
+    using QuizHut.Infrastructure.Commands;
+    using QuizHut.Infrastructure.Services.Contracts;
     using QuizHut.ViewModels.Base;
 
     internal class StudentRegistrationViewModel : ViewModel
     {
+        private INavigationService navigationService;
+
+        public INavigationService NavigationService
+        {
+            get => navigationService;
+            set => Set(ref navigationService, value);
+        }
+
+        public StudentRegistrationViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+
+            NavigateAuthorizationViewCommand = new ActionCommand(OnNavigateAuthorizationViewCommandExecuted, CanNavigateAuthorizationViewCommandExecute);
+        }
+
+        #region NavigateAuthorizationViewCommand
+
+        public ICommand NavigateAuthorizationViewCommand { get; }
+
+        private void OnNavigateAuthorizationViewCommandExecuted(object p) { NavigationService.NavigateTo<AuthorizationViewModel>(); }
+
+        private bool CanNavigateAuthorizationViewCommandExecute(object p) => true;
+
+        #endregion
     }
 }
