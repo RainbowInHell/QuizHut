b0f605be868874f52c2618714a9b32423e205476
Ssparf <sashaparfiak007@gmail.com>
2023-04-20 13:50:53 +0300

    Added Tag property in bindablPasswordBox

--- QuizHut/QuizHut/Infrastructure/UserControls/BindablePasswordBox.xaml.cs
+++ QuizHut/QuizHut/Infrastructure/UserControls/BindablePasswordBox.xaml.cs
@@ -8,10 +8,20 @@
         public static readonly DependencyProperty PasswordProperty =
             DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox));
 
+        public static readonly DependencyProperty TagProperty =
+        DependencyProperty.Register("Tag", typeof(object), typeof(BindablePasswordBox),
+            new PropertyMetadata(null, TagPropertyChanged));
+
         public string Password
         {
             get => (string)GetValue(PasswordProperty);
             set => SetValue(PasswordProperty, value);
+        }
+
+        public object Tag
+        {
+            get => GetValue(TagProperty);
+            set => SetValue(TagProperty, value);
         }
 
         public BindablePasswordBox()
@@ -24,6 +34,14 @@
         private void OnPasswordChanged(object sender, RoutedEventArgs e)
         {
             Password = bindablePassword.Password;
+        }
+
+        private static void TagPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is BindablePasswordBox passwordBox)
+            {
+                passwordBox.bindablePassword.Tag = e.NewValue;
+            }
         }
     }
 }
