c59adc4260f2af6a3ae61c2b64bb4648fb54da3d
Ssparf <sashaparfiak007@gmail.com>
2023-04-25 18:51:09 +0300

    Added Close, Maximize and Minimize control buttons to MainView

---
+++ QuizHut/QuizHut/Infrastructure/Commands/MaximizeApplicationCommand.cs
@@ -0,0 +1,19 @@
+﻿namespace QuizHut.Infrastructure.Commands
+{
+    using System.Windows;
+
+    using QuizHut.Infrastructure.Commands.Base;    
+
+    class MaximizeApplicationCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter)
+        {
+            if (Application.Current.Windows[0].WindowState == WindowState.Normal)
+                Application.Current.Windows[0].WindowState = WindowState.Maximized;
+            else
+                Application.Current.Windows[0].WindowState = WindowState.Normal;
+        }
+    }
+}
